cbfcdbf456be3f46a9e012bdee0ca964ecd62650
ryo ariyama <ariyama@sansan.com>
2021-01-06 22:41:35 +0900

    improved solution_191

--- argorithm/191_number_of_1_bits.py
+++ argorithm/191_number_of_1_bits.py
@@ -4,3 +4,16 @@ class Solution:
         num_arr = list(str(temp))
         arr = [i for i in num_arr if i != '0']
         return len(arr)
+
+class ImprovedSolution:
+    def hammingWeight(self, n: int) -> int:
+        temp = "{:032b}".format(n)
+        num_arr = list(str(temp))
+        num_arr.sort()
+        cnt = 0
+        for i in reversed(num_arr):
+            if i == '1':
+                cnt += 1
+            else:
+                return cnt
+        return cnt
